840a803896d159144f0fbfe5ecd5cc639e7cffb6
MaxinAIAvto <avto.bolashvili@maxinai.com>
2020-11-21 23:55:12 +0400

    Implement search functionality

--- adjaraParser.js
+++ adjaraParser.js
@@ -3,24 +3,89 @@
 const {parse} = require('url');
 const {send, streamFile} = require('./requests');
 
+function mapQuality(quality) {
+    switch (quality) {
+        case "HD":
+            return "HIGH";
+        case "SD":
+            return "MEDIUM";
+        default:
+            return quality;
+    }
+}
+
 function parseUrl(url) {
     const parsed = parse(url);
     const parts = parsed.pathname.split("/").filter(p => p.length > 0);
+    const params = parsed.query.split("&");
+    const langParam = params[0].split("=");
+    const qualityParam = params[1].split("=");
+    let lang, quality;
 
-    return parts[1];
+    if (langParam && langParam.length > 0) {
+        lang = langParam[1];
+    }
+
+    if (qualityParam && qualityParam.length > 0) {
+        quality = mapQuality(qualityParam[1]);
+    }
+
+    return {
+        id: parts[1],
+        name: parts[2],
+        lang: lang,
+        quality: quality
+    };
+}
+
+function parseFileUrl(url) {
+    const parsed = parse(url);
+    const parts = parsed
+        .pathname
+        .split("/")
+        .filter(p => p.length > 0)
+        .filter(p => !isNaN(Number(p)));
+
+    return {
+        movieId: parts[0],
+        fileId: parts[1]
+    }
 }
 
 async function fetch(url) {
-    const id = parseUrl(url);
+    const details = parseUrl(url);
+    const {id, name, lang, quality} = details;
     const body = JSON.parse(await send(`/api/v1/movies/${id}?source=adjaranet`));
-    const fileBody = await send(`/api/v1/movies/${body.data.id}/season-files/0?source=adjaranet`);
-    console.info(JSON.parse(fileBody).data[0].files[0].files[0]);
+    const fileBody = JSON.parse(await send(`/api/v1/movies/${body.data.id}/season-files/0?source=adjaranet`));
+    const files = fileBody.data[0].files;
+    let byLang, byQuality;
 
-    return JSON.parse(fileBody).data[0].files;
+    if (lang) {
+        byLang = files.find(file => file.lang === lang);
+    } else {
+        byLang = fileBody.data[0].files[0];
+    }
+
+    if (quality) {
+        byQuality = byLang.files.find(file => file.quality === quality);
+    } else {
+        byQuality = byLang.files[0];
+    }
+
+    const {movieId, fileId} = parseFileUrl(byQuality.src);
+
+    return {
+        movieId,
+        fileId,
+        name,
+        duration: byQuality.duration,
+        quality: byQuality.quality,
+        language: byLang.lang
+    };
 }
 
 function download(response, movieId, fileId) {
-     streamFile(response, `/api/v1/movies/${movieId}/files/${fileId}`);
+    streamFile(response, `/api/v1/movies/${movieId}/files/${fileId}`);
 }
 
 module.exports = {
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 
+const xhr = new XMLHttpRequest();
+
+const HOSTNAME = 'http://localhost:3000';
+
 function downloadImage(url, name) {
-    let xhr = new XMLHttpRequest();
     xhr.open('POST', url, true);
     xhr.responseType = 'blob';
     xhr.onload = function() {
@@ -20,5 +23,37 @@ function downloadImage(url, name) {
     xhr.send(JSON.stringify({
         movieId: "878496841",
         fileId: "1397146"
+    }));
+}
+
+function searchMovie() {
+    const input = document.getElementById('search').value;
+    if (input && input.length) {
+        sendSearch(input);
+    }
+}
+
+function sendSearch(url) {
+    xhr.open('POST', `${HOSTNAME}/api/search`, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.responseType = 'text';
+    xhr.onload = function() {
+        const response = JSON.parse(this.response);
+        const resultDiv = document.getElementById('searchResult');
+        const name = document.getElementById("name");
+        const quality = document.getElementById("quality");
+        const language = document.getElementById("language");
+        const duration = document.getElementById("duration");
+        name.innerText = response.name;
+        quality.innerText = response.quality;
+        language.innerText = response.language;
+        duration.innerText = response.duration;
+        resultDiv.style.display = "block";
+    };
+    xhr.onerror = err => {
+        alert('Failed get movie data');
+    };
+    xhr.send(JSON.stringify({
+        "url": url
     }));
 }
